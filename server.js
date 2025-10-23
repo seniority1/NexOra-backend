a27929d26aa1351c08e2f7cd20af759958ff219c
seniority1 <quicktaskteam2025@gmail.com>
2025-10-23 21:37:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
+import authRoutes from "./routes/auth.js";
 
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+// ⚙️ Connect routes
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => res.send("🚀 NexOra backend is running!"));
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
