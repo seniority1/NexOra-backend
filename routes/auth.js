c3f963ab9339b1a5fe10f0ab2b646d8315ce86ce
seniority1 <quicktaskteam2025@gmail.com>
2025-10-23 21:36:12 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,9 @@
+import express from "express";
+import { register } from "../controllers/authController.js";
 
+const router = express.Router();
+
+// 🪄 Register route
+router.post("/register", register);
+
+export default router;
