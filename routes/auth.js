8fe1717dddf3a779f2327e4db16ac05e5761a7fe
seniority1 <quicktaskteam2025@gmail.com>
2025-10-29 11:24:46 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,15 @@
-import express from "express";
 import {
   register,
   verifyCode,
   resendVerificationCode,
   login,
+  forgotPassword,
+  resetPassword,
 } from "../controllers/authController.js";
 
-const router = express.Router();
-
-// 🟢 Auth routes
 router.post("/register", register);
 router.post("/verify", verifyCode);
 router.post("/resend", resendVerificationCode);
 router.post("/login", login);
-
-export default router;
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
