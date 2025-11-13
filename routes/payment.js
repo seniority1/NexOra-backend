b9ecfceb9aeda9381ebad30bbb30c21e4a78b739
seniority1 <quicktaskteam2025@gmail.com>
2025-11-13 17:36:08 +0100

    Create payment.js

---
+++ routes/payment.js
@@ -0,0 +1,9 @@
+// routes/payment.js
+import express from "express";
+import { verifyAndCreditCoins } from "../controllers/paymentController.js";
+
+const router = express.Router();
+
+router.post("/verify", verifyAndCreditCoins);
+
+export default router;
