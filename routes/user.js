5eea17740377ecccf9dc3b030a026de35bb25bed
seniority1 <quicktaskteam2025@gmail.com>
2025-11-25 21:37:21 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,21 @@
 import express from "express";
-import { getUserInfo, updateCoins, addDeployment } from "../controllers/userController.js";
+import {
+  getUserInfo,
+  updateCoins,
+  addDeployment,
+  getTransactions,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 // 🧠 Get user info
 router.get("/info", getUserInfo);
 
-// 💰 Update coins
+// 💰 Update coin balance + log transaction
 router.post("/updateCoins", updateCoins);
+
+// 📜 Get transaction history
+router.get("/transactions", getTransactions);
 
 // 🚀 Add a new deployment
 router.post("/addDeployment", addDeployment);
